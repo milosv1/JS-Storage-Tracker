e8a53f257466e1f0778331ef1f27211c250fb39f
Milos <milosvuksanovic53@gmail.com>
2021-02-01 20:09:42 +1100

    First code commit.

--- js_t.js
+++ js_t.js
@@ -1,3 +1,10 @@
 //Experimental side project for JavaScript,
 //The use of this is to use JavaScript to grab system related information then output it,
 //The main purpose of it, is to learn its concepts and make it fun.
+//This will be using a Library called systeminformation.
+const s_i = require('systeminformation');
+
+s_i.cpu()
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
+
