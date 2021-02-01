291efc15deebb5ea010d31e609766b2794bc91ea
Milos <milosvuksanovic53@gmail.com>
2021-02-01 20:03:30 +1100

    Created js_t.js
    First commit and little description.

---
+++ js_t.js
@@ -0,0 +1,3 @@
+//Experimental side project for JavaScript,
+//The use of this is to use JavaScript to grab system related information then output it,
+//The main purpose of it, is to learn its concepts and make it fun.
